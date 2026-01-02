cdd2eec4ab05a32d6a6ac18775568dd685ec7cf9
SirShabluli <eyalmordechai@gmail.com>
2026-01-02 16:32:06 +0530

    added LinkedIn link just to see wtf is up

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
@@ -5,6 +7,14 @@ export default function Home() {
       <p className="mt-4 text-gray-400 font-light">
         Something creative is loading from India...
       </p>
+      <a
+        href="https://www.linkedin.com/in/eyal-mo"
+        target="_blank"
+        rel="noreferrer"
+        className="mt-6 underline text-sm"
+      >
+        Here&apos;s a link to my LinkedIn
+      </a>
     </main>
   );
 }
