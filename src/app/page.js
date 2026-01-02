432d1dab3cf7994d3cebdfe5d44fba1f2c0bd17c
SirShabluli <eyalmordechai@gmail.com>
2026-01-02 16:06:10 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white">
+      <h1 className="text-6xl font-bold tracking-tighter italic">EYAL.WORKS</h1>
+      <p className="mt-4 text-gray-400 font-light">
+        Something creative is loading from India...
+      </p>
+    </main>
+  );
+}
