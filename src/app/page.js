41fc9637fc7ff21d7ec16a380dee2356620afec3
SirShabluli <eyalmordechai@gmail.com>
2026-01-13 15:54:28 +0530

    added basic layout

--- src/app/page.js
+++ src/app/page.js
@@ -25,15 +25,34 @@ export default function Home() {
           </a>
         </section>
         <section>
-          <div className="grid grid-cols-12 gap-4 flex justify-center">
-            <div className="col-span-4 md:col-span-3 outline outline-1 outline-red-500 md:col-start-2">
-              LEFT CONTENT
+          <div className="grid grid-cols-12 gap-8 flex justify-center my-20 min-h-screen ">
+            <div className="col-span-4 md:col-span-3 outline outline-1 outline-red-500 md:col-start-2 flex flex-col justify-center gap-5">
+              <p>
+                Technical errors and verification loops are high-friction points
+                that lead to app abandonment. I chose to address this through
+                the literal interpretation of a common dating term:
+              </p>
+              <h3>Catfish</h3>
+              <p>
+                By illustrating a grumpy cat wearing a fish hat, the app takes a
+                self-deprecating tone. This "wink" at the user transforms a
+                moment of potential anger ("Why won't it verify me?") into a
+                moment of levity. It humanizes the technology, suggesting that
+                the error might be a "fishy" mistake on the system's part rather
+                than a failure of the user, making them much more likely to try
+                again.
+              </p>
             </div>
             <div className="col-span-4 md:col-span-4 md:col-start-5 outline outline-1 outline-red-500">
               MIDDLE
             </div>
-            <div className="col-span-4 md:col-span-3 md:col-start-9 outline outline-1 outline-red-500 quote">
-              RIGHT CONTENT
+            <div className="col-span-4 md:col-span-3 md:col-start-9 outline outline-1 outline-red-500 flex flex-col">
+              <h2>Defusing Frustration</h2>
+              <p>Using a "Pun" as a tool for de-escalation.</p>
+              <span className="mt-10 quote">
+                &ldquo;Something smells fishy, but it might just be our
+                mistake&ldquo;
+              </span>
             </div>
           </div>
         </section>
