4d4da7835c145e4c6c441f9b7f4f9f54b6473646
SirShabluli <eyalmordechai@gmail.com>
2026-03-27 18:01:57 +0700

    trying to change from dvh to svh for less buggy

--- src/app/components/MobilePhoneShowcase.js
+++ src/app/components/MobilePhoneShowcase.js
@@ -41,7 +41,7 @@ export default function MobilePhoneShowcase({
   const steps = ["Challenge", section.screenNam || "Screen", "Solution"];
 
   return (
-    <div className="flex flex-col h-screen-dvh" style={{ backgroundColor: bgColor }}>
+    <div className="flex flex-col" style={{ height: "100svh", backgroundColor: bgColor }}>
       <Swiper
         direction="horizontal"
         initialSlide={0}
